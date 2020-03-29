aa17efa411891c545179e412bcfb0f4e90af0f35
Danilo Campos <daniloblk@gmail.com>
2020-03-29 20:20:37 +0200

    2020

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -11,8 +11,8 @@
   </div>
 
   <div class="product-description grid3">
-    <img class="product-image-detail" src="~assets/image-product-detail.jpg" />
-    <img class="product-image-scene" src="~assets/image-produtc-scene.png" />
+    <img class="product-image-detail" :src="blogPost.imagedetail" />
+    <img class="product-image-scene" :src="blogPost.imageplacement" />
     <div class="product-description-title graphik24">{{blogPost.productdescriptiontitle}}</div>
     <div class="product-description-items-left graphik16 gray">
       <ul>
@@ -29,6 +29,34 @@
   <div class="product-technical-informantion grid3">
     <div class="div-line"></div>
     <div class="product-technical-informantion-title pressura40">Informações técnicas</div>
+    <div class="product-technical-informantion-specs-img"><img src="~assets/image-5.jpg" /></div>
+    <div class="product-technical-informantion-specs">
+      <ul>
+        <li class="product-technical-informantion-specs-list-items"><span class="pressura15 gray">Dimensões</span> <span class="pressura15" style="float:right;">h 720 x b 620 x d 530 mm</span></li>
+        <li class="product-technical-informantion-specs-list-items"><span class="pressura15 gray">Acabamento</span> <span class="pressura15" style="float:right;">Madeira e aço</span></li>
+      </ul>
+    </div>
+    <div class="product-technical-informantion-specs-downloads">
+      <ul>
+        <li class="product-technical-informantion-specs-list-items"><span class="pressura15 pink">Download de catálogo</span> <span class="pressura15 pink" style="float:right;">↓</span></li>
+        <li class="product-technical-informantion-specs-list-items"><span class="pressura15 pink">Download de informações básicas</span> <span class="pressura15 pink" style="float:right;">↓</span></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="product-module-services grid3">
+    <div class="div-line"></div>
+    <div class="product-module-services-title pressura60">serviços prestados aos nossos clientes, desde o primeiro contato comercial até a pós-ocupação</div>
+    <ul class="product-module-services-list-left graphik18">
+      <li>Design & Specification</li>
+      <li>Design & Specification</li>
+      <li>Design & Specification</li>
+    </ul>
+    <ul class="product-module-services-list-right graphik18">
+      <li>Installation</li>
+      <li>Design & Specification</li>
+      <li>Design & Specification</li>
+    </ul>
   </div>
 </div>
 </template>
@@ -87,15 +115,67 @@
     margin-bottom: 16px;
   }
 
+  .product-technical-informantion {
+    margin-bottom: 81px;
+  }
+
   .div-line {
     grid-column: 1/4;
+    grid-row: 1;
     height: 1px;
     background-color: #DEDCDB;
     margin-bottom: 21px;
   }
 
-  .product-technical-informantion-title{
+  .product-technical-informantion-title {
     grid-column: 1/2;
+    grid-row: 2;
+  }
+
+  .product-technical-informantion-specs {
+    grid-column: 2/3;
+    grid-row: 3;
+  }
+
+  .product-technical-informantion-specs-img {
+    grid-column: 2/3;
+    grid-row: 2;
+    margin: 25px 0 27px 0;
+  }
+
+  .product-technical-informantion-specs-list-items {
+    padding: 14px 0 17px 0;
+    border-top: solid 1px #DEDCDB;
+  }
+
+  .product-technical-informantion-specs-downloads {
+    grid-column: 3/4;
+    grid-row: 3;
+  }
+
+  .product-module-services {
+    margin-bottom: 81px;
+  }
+
+  .product-module-services-title {
+    grid-column: 1/3;
+    margin-bottom: 59px;
+  }
+
+  .product-module-services-list-left {
+    grid-column: 1/2;
+  }
+
+  .product-module-services-list-left li {
+    padding-bottom: 4px;
+  }
+
+  .product-module-services-list-right {
+    grid-column: 2/3;
+  }
+
+  .product-module-services-list-right li {
+    padding-bottom: 4px;
   }
 </style>
 
