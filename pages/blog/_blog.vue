8a8658432c70f8b82a57372a15c221a37c46eb60
Danilo Campos <daniloblk@gmail.com>
2020-03-29 21:01:24 +0200

    2101

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -16,7 +16,7 @@
     <div class="product-description-title graphik24">{{blogPost.productdescriptiontitle}}</div>
     <div class="product-description-items-left graphik16 gray">
       <ul>
-        <li>{{blogPost.productdescriptionlist}}</li>
+        <li>{{blogPost.descriptionitems}}</li>
       </ul>
     </div>
     <div class="product-description-items-right graphik16 gray">
