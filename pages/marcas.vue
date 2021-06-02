a52a835c19de7fb665f8d74d2cf41b8532090302
Danilo Campos <daniloblk@gmail.com>
2021-06-02 20:29:13 +0200

    textos

--- pages/marcas.vue
+++ pages/marcas.vue
@@ -3,7 +3,6 @@
     <MenuMobile />
 
     <div class="grid3">
-
       <!-- MARCAS -->
 
       <h1 class="nossas-marcas-title pressura60">
@@ -11,204 +10,321 @@
       </h1>
 
       <h2 class="nossas-marcas-description graphik24">
-        Além de uma extensa linha própria de serviços e produtos, a Escinter é Dealer de algumas das principais marcas internacionais de design no Brasil. Por isso somos capazes de fornecer todos os produtos e serviços relacionados a estas parcerias. Dentre estas grandes marcas está a Knoll, e juntos desenvolvemos vários projetos para o Brasil, garantindo para o nosso país as mesmas soluções das outras regiões do globo.
+        Além de uma extensa linha própria de serviços e produtos, a Escinter é
+        Dealer de algumas das principais marcas internacionais de design no
+        Brasil. Por isso somos capazes de fornecer todos os produtos e serviços
+        relacionados a estas parcerias. Dentre estas grandes marcas está a
+        Knoll, e juntos desenvolvemos vários projetos para o Brasil, garantindo
+        para o nosso país as mesmas soluções das outras regiões do globo.
       </h2>
 
       <div></div>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Knoll</h3></li>
-        <li class="graphik18 gray"><p>Desde sua fundação, em 1938, a Knoll é reconhecida internacionalmente por criar móveis que inspiram, evoluem e perduram. Seu compromisso com a inovação e design moderno tem produzido um portfólio abrangente de sistemas, cadeiras, arquivos, mesas, armários e acessórios.
-          <br><br>Os fundadores, Hans e Florence Knoll, abraçaram o espírito criativo da Bauhaus School e da Cranbrook Academy of Art para criar novos tipos de móveis e ambientes para o local de trabalho. Sua abordagem, em que o artesanato se junta à tecnologia por meio do uso do design, ancora nossa perspectiva e molda os valores que buscamos viver hoje.
-          <br><br>
-            Uma longa história de colaborações, de modernistas pioneiros a designers contemporâneos ousados, define não apenas o passado, mas o futuro da Knoll por meio de parcerias ativas, recentes e futuras com David Rockwell, Antenna Design, Barber Osgerby, Formway Design, Don Chadwick, David Adjaye e Rem Koolhaas.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            Desde sua fundação, em 1938, a Knoll é reconhecida
+            internacionalmente por criar móveis que inspiram, evoluem e
+            perduram. Seu compromisso com a inovação e design moderno tem
+            produzido um portfólio abrangente de sistemas, cadeiras, arquivos,
+            mesas, armários e acessórios. <br /><br />Os fundadores, Hans e
+            Florence Knoll, abraçaram o espírito criativo da Bauhaus School e da
+            Cranbrook Academy of Art para criar novos tipos de móveis e
+            ambientes para o local de trabalho. Sua abordagem, em que o
+            artesanato se junta à tecnologia por meio do uso do design, ancora
+            nossa perspectiva e molda os valores que buscamos viver hoje.
+            <br /><br />
+            Uma longa história de colaborações, de modernistas pioneiros a
+            designers contemporâneos ousados, define não apenas o passado, mas o
+            futuro da Knoll por meio de parcerias ativas, recentes e futuras com
+            David Rockwell, Antenna Design, Barber Osgerby, Formway Design, Don
+            Chadwick, David Adjaye e Rem Koolhaas.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Kush+co</h3></li>
-        <li class="graphik18 gray"><p>Com mais de 60 anos de história, a Kusch+Co representa a união entre tecnologia e qualidade na produção de móveis para ambientes internos. Suas peças são desenvolvidas de acordo com os padrões mais elevados do mercado internacional e preenchem de maneira singular as necessidades de seus clientes.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            Com mais de 60 anos de história, a Kusch+Co representa a união entre
+            tecnologia e qualidade na produção de móveis para ambientes
+            internos. Suas peças são desenvolvidas de acordo com os padrões mais
+            elevados do mercado internacional e preenchem de maneira singular as
+            necessidades de seus clientes.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Humanscale</h3></li>
-        <li class="graphik18 gray"><p>Humanscale é uma empresa focada em produtos ergonômicos que melhoram a saúde e conforto no trabalho. Inovações prestigiadas com mais de 200 prêmios e com destaque em diversos museus.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            Humanscale é uma empresa focada em produtos ergonômicos que melhoram
+            a saúde e conforto no trabalho. Inovações prestigiadas com mais de
+            200 prêmios e com destaque em diversos museus.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Escinter</h3></li>
-        <li class="graphik18 gray"><p>A nossa linha própria de produtos. A Escinter é uma empresa familiar com 30 anos de atuação no mercado de interiores de espaços corporativos. Nós acreditamos que o investimento na adequação de áreas de trabalho, promove o aumento na qualidade de vida dos funcionários, com melhoria em sua saúde, bem estar e produtividade.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            A nossa linha própria de produtos. A Escinter é uma empresa familiar
+            com 30 anos de atuação no mercado de interiores de espaços
+            corporativos. Nós acreditamos que o investimento na adequação de
+            áreas de trabalho, promove o aumento na qualidade de vida dos
+            funcionários, com melhoria em sua saúde, bem estar e produtividade.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>F.WAY</h3></li>
-        <li class="graphik18 gray"><p>A F.WAY é a marca que está liderando a nova geração de assentos corporativos. Nossas linhas "Red", "Contract" e "Life" atendem diversas necessidades, ambientes e perfis diferentes. Criando soluções corporativas, com ambientes versáteis, comunicativos e que facilitam as relações entre as várias áreas de uma empresa.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            A F.WAY é a marca que está liderando a nova geração de assentos
+            corporativos. Nossas linhas "Red", "Contract" e "Life" atendem
+            diversas necessidades, ambientes e perfis diferentes. Criando
+            soluções corporativas, com ambientes versáteis, comunicativos e que
+            facilitam as relações entre as várias áreas de uma empresa.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Maxdesign</h3></li>
-        <li class="graphik18 gray"><p>Maxdesign é uma marca empenhada em investigar a configuração dos espaços, desde os públicos até escritórios e casas particulares. O ambiente criado pelo homem é um sistema em rápida evolução que busca novas éticas e conexões. O núcleo da nossa abordagem é ouvir as práticas do usuário e os objetivos do designer. Como os espaços estão em constante mudança, cada ferramenta deve ser capaz de se adaptar e encontrar novas expressões. Por meio de inovação e pesquisa, pretendemos desenvolver produtos para apoiar clientes e profissionais na realização de soluções eficazes e ecossistemas dinâmicos. Nosso objetivo é entender o que é necessário e oferecer soluções inspiradoras e versáteis em troca. A Maxdesign cria produtos para as pessoas e seus ambientes.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            Maxdesign é uma marca empenhada em investigar a configuração dos
+            espaços, desde os públicos até escritórios e casas particulares. O
+            ambiente criado pelo homem é um sistema em rápida evolução que busca
+            novas éticas e conexões. O núcleo da nossa abordagem é ouvir as
+            práticas do usuário e os objetivos do designer. Como os espaços
+            estão em constante mudança, cada ferramenta deve ser capaz de se
+            adaptar e encontrar novas expressões. Por meio de inovação e
+            pesquisa, pretendemos desenvolver produtos para apoiar clientes e
+            profissionais na realização de soluções eficazes e ecossistemas
+            dinâmicos. Nosso objetivo é entender o que é necessário e oferecer
+            soluções inspiradoras e versáteis em troca. A Maxdesign cria
+            produtos para as pessoas e seus ambientes.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Muuto</h3></li>
-        <li class="graphik18 gray"><p>A Muuto está enraizada na tradição de design escandinavo caracterizada por uma estética duradoura, funcionalidade, habilidade artesanal e uma expressão honesta.
-          <br><br>Expandindo esta herança com materiais, técnicas e pensamento criativo inovadores, nossa ambição é oferecer novas perspectivas sobre o design escandinavo. Na verdade, nosso nome, Muuto, vem de muutos, que significa nova perspectiva em finlandês. Selecionamos os principais designers contemporâneos que são fortes intérpretes de nossa filosofia e combinamos seus talentos com a apaixonada equipe criativa da Muuto. Você conhecerá nosso design porque ele tem muutos: objetos tornados sublimes por meio de novas perspectivas, apreciados em todo o mundo, representando o melhor do design escandinavo hoje.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            A Muuto está enraizada na tradição de design escandinavo
+            caracterizada por uma estética duradoura, funcionalidade, habilidade
+            artesanal e uma expressão honesta. <br /><br />Expandindo esta
+            herança com materiais, técnicas e pensamento criativo inovadores,
+            nossa ambição é oferecer novas perspectivas sobre o design
+            escandinavo. Na verdade, nosso nome, Muuto, vem de muutos, que
+            significa nova perspectiva em finlandês. Selecionamos os principais
+            designers contemporâneos que são fortes intérpretes de nossa
+            filosofia e combinamos seus talentos com a apaixonada equipe
+            criativa da Muuto. Você conhecerá nosso design porque ele tem
+            muutos: objetos tornados sublimes por meio de novas perspectivas,
+            apreciados em todo o mundo, representando o melhor do design
+            escandinavo hoje.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Lady</h3></li>
-        <li class="graphik18 gray"><p>Fundada em 1940 por um casal de austríacos que prezavam pela evolução constante e pela inovação, a Tecelagem Lady mantém esses valores vivos na companhia até hoje. A empresa com sede em São Paulo está no mercado de revestimentos como uma grande fornecedora de soluções para diversos ambientes – comerciais, corporativos, hoteleiros e residenciais, recentemente se notabilizando também pelos materiais acústicos para ambientes corporativos</p></li>
+        <li class="graphik18 gray">
+          <p>
+            Fundada em 1940 por um casal de austríacos que prezavam pela
+            evolução constante e pela inovação, a Tecelagem Lady mantém esses
+            valores vivos na companhia até hoje. A empresa com sede em São Paulo
+            está no mercado de revestimentos como uma grande fornecedora de
+            soluções para diversos ambientes – comerciais, corporativos,
+            hoteleiros e residenciais, recentemente se notabilizando também
+            pelos materiais acústicos para ambientes corporativos
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Inwall</h3></li>
-        <li class="graphik18 gray"><p>A InWall é uma empresa nacional especializada na fabricação, projeto e execução de divisórias acústicas piso teto. Incluir inWall no seu projeto é a garantia de qualidade superior, excelente acabamento, múltiplas possibilidades de personalização e pontualidade nos prazos de entrega.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            A InWall é uma empresa nacional especializada na fabricação, projeto
+            e execução de divisórias acústicas piso teto. Incluir inWall no seu
+            projeto é a garantia de qualidade superior, excelente acabamento,
+            múltiplas possibilidades de personalização e pontualidade nos prazos
+            de entrega.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas">
         <li class="marca-item-title pressura40"><h3>Beltech</h3></li>
-        <li class="graphik18 gray"><p>A Beltech colabora com arquitetos, design de interiores, clientes corporativos e redes hoteleiras para encontrar soluções inovadoras de pisos, oferencendo marcas como Shaw Contract.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            A Beltech, desde 1993, fornece carpetes e pisos vinílicos para os
+            segmentos corporativo e hoteleiro. Hoje, são importações de mais de
+            7 países e 3 continentes, com o que há de melhor em marcas, design,
+            qualidade, tecnologia, sustentabilidade e preço. No portfólio da
+            Beltech estão marcas reconhecidas mundialmente: Mohawk Group,
+            Durkan, Shaw Contract, Shaw Hospitality, Crossley, Patcraft e J&J
+            Flooring.
+          </p>
+        </li>
       </ul>
 
       <ul class="marcas last">
         <li class="marca-item-title pressura40"><h3>Sollos</h3></li>
-        <li class="graphik18 gray"><p>Desde 2004, percebemos que para crescer era preciso investir em conceito, identidade e qualidade. Desde então, optamos por utilizar o design como ferramenta estratégica e elemento integrador de toda nossa cadeia produtiva. Essa decisão fez com que a SOLLOS ficasse conhecida como uma indústria inovadora. Buscamos integrar o melhor de nossa cultura às referências internacionais através do uso racional da madeira como matéria-prima, do controle dos processos e métodos de produção em concordância com todos os requisitos ecológicos e do investimento em equipamentos de última geração aliados a técnicas artesanais.</p></li>
+        <li class="graphik18 gray">
+          <p>
+            Desde 2004, percebemos que para crescer era preciso investir em
+            conceito, identidade e qualidade. Desde então, optamos por utilizar
+            o design como ferramenta estratégica e elemento integrador de toda
+            nossa cadeia produtiva. Essa decisão fez com que a SOLLOS ficasse
+            conhecida como uma indústria inovadora. Buscamos integrar o melhor
+            de nossa cultura às referências internacionais através do uso
+            racional da madeira como matéria-prima, do controle dos processos e
+            métodos de produção em concordância com todos os requisitos
+            ecológicos e do investimento em equipamentos de última geração
+            aliados a técnicas artesanais.
+          </p>
+        </li>
       </ul>
-
-
     </div>
   </div>
 </template>
 
 <style scoped>
 .carousel-cell {
-  width: calc( ( 100% - 0px ) / 3);
+  width: calc((100% - 0px) / 3);
 }
 
-  .content {
-    padding-top: 415px;
-    margin-bottom: 158px;
-  }
+.content {
+  padding-top: 415px;
+  margin-bottom: 158px;
+}
 
-  .servicos-title {
-    grid-column: 1/3;
-    margin-bottom: 32px;
+.servicos-title {
+  grid-column: 1/3;
+  margin-bottom: 32px;
+}
+
+.linha-div {
+  grid-column: 1/4;
+  height: 1px;
+  background-color: #dedcdb;
+}
+
+.sobre-description {
+  margin: 38px 0 34px 0;
+}
+
+.showroom-adress {
+  grid-column: 1/2;
+  align-self: end;
+  padding-bottom: 40px;
+}
+
+.img-showroom {
+  grid-column: 2/4;
+  margin-bottom: 40px;
+}
+
+.nossas-marcas-title {
+  grid-column: 1/3;
+  margin: 31px 0 27px 0;
+}
+
+.nossas-marcas-description {
+  grid-column: 1/3;
+  margin-bottom: 123px;
+}
+
+.marcas {
+  margin-bottom: 80px;
+}
+
+.last {
+  margin-bottom: 80px;
+}
+
+.marca-item-title {
+  margin-bottom: 20px;
+}
+
+.clientes-title {
+  grid-column: 1/3;
+  margin: 31px 0 81px 0;
+}
+
+.cliente-grid {
+  grid-column: 1/4;
+  margin-bottom: 80px;
+}
+
+.carousel-pag {
+  grid-column: 1/4;
+}
+
+.grid3-projetos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 28px;
+}
+
+@media screen and (max-width: 1020px) {
+  .carousel-cell {
+    width: calc(100vw - 40px);
+    margin-right: 20px;
   }
 
   .linha-div {
-    grid-column: 1/4;
-    height: 1px;
-    background-color: #DEDCDB;
+    grid-column: 1/3;
   }
 
   .sobre-description {
-    margin: 38px 0 34px 0;
+    grid-column: 1/3;
+    margin: 38px 0 0px 0;
   }
 
   .showroom-adress {
-    grid-column: 1/2;
-    align-self: end;
-    padding-bottom: 40px;
+    grid-column: 1/3;
+    grid-row: 6;
   }
 
   .img-showroom {
-    grid-column: 2/4;
-    margin-bottom: 40px;
-  }
-
-  .nossas-marcas-title {
-    grid-column: 1/3;
-    margin: 31px 0 27px 0;
-  }
-
-  .nossas-marcas-description {
-    grid-column: 1/3;
-    margin-bottom: 123px;
-  }
-
-  .marcas {
-    margin-bottom: 80px;
-  }
-
-  .last {
-    margin-bottom: 80px;
-  }
-
-  .marca-item-title {
-    margin-bottom: 20px;
-  }
-
-  .clientes-title {
-    grid-column: 1/3;
-    margin: 31px 0 81px 0;
-  }
-
-  .cliente-grid {
-    grid-column: 1/4;
-    margin-bottom: 80px;
-  }
-
-  .carousel-pag {
-    grid-column: 1/4;
-  }
-  
-  .grid3-projetos {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      grid-column-gap: 28px;
-  }
-
-  @media screen and (max-width: 1020px) {
-
-.carousel-cell {
-  width: calc(100vw - 40px);
-  margin-right: 20px;
-}
-
-
-    .linha-div {
-      grid-column: 1/3;
-    }
-
-    .sobre-description {
-      grid-column: 1/3;
-      margin: 38px 0 0px 0;
-    }
-
-    .showroom-adress {
-        grid-column: 1/3;
-        grid-row: 6;
-    }
-
-    .img-showroom {
     grid-column: 1/3;
     margin-top: 40px;
     margin-bottom: 16px;
-    }
+  }
 
-    .marcas {
-      grid-column: 1/3;
-      margin-bottom: 64px;
-    }
-
-    .cliente-grid {
-      grid-column: 1/3; 
-    }
-
-    .nossas-marcas-description {
+  .marcas {
+    grid-column: 1/3;
     margin-bottom: 64px;
   }
 
-    .content {
-      padding-top: 157px;
-    }
-
+  .cliente-grid {
+    grid-column: 1/3;
   }
+
+  .nossas-marcas-description {
+    margin-bottom: 64px;
+  }
+
+  .content {
+    padding-top: 157px;
+  }
+}
 </style>
 
 <script>
-import MenuMobile from '~/components/MenuMobile.vue'
-import ProjetoItem from '~/components/ProjetoItem.vue'
+import MenuMobile from "~/components/MenuMobile.vue";
+import ProjetoItem from "~/components/ProjetoItem.vue";
 
 export default {
   components: {
@@ -217,13 +333,13 @@ export default {
   },
   data() {
     return {
-      title: 'Sobre'
-    }
+      title: "Sobre"
+    };
   },
   head() {
     return {
       title: this.title
-    }
+    };
   }
-}
+};
 </script>
--- pages/sobre.vue
+++ pages/sobre.vue
@@ -3,17 +3,32 @@
     <MenuMobile />
 
     <div class="grid3">
-      <h1 class="servicos-title pressura60">A Escinter é uma empresa de soluções integradas para ambientes corporativos.</h1>
-      <h2 class="servicos-title graphik24 gray">Em 30 anos de atuação, entendemos as necessidades de diversos perfis de clientes e dispomos de um extenso portfólio de produtos e serviços desenvolvidos por nós e por algumas das principais marcas internacionais do design.</h2>
+      <h1 class="servicos-title pressura60">
+        A Escinter é uma empresa familiar fundada em 1991 com propósito de
+        contribuir na construção de espaços corporativos de qualidade.
+      </h1>
+      <h2 class="servicos-title graphik24 gray">
+        Nós acreditamos que o investimento na adequação destas áreas de
+        trabalho, promove o aumento na qualidade de vida dos funcionários,
+        melhorando sua saúde, bem estar e produtividade.
+      </h2>
 
       <div class="linha-div"></div>
 
       <p class="sobre-description graphik18">
-      Nós acreditamos que o investimento na adequação de áreas de trabalho, promove o aumento na qualidade de vida dos funcionários, com melhoria em sua saúde, bem estar e produtividade, e a corporação valoriza seus colaboradores tornando o ambiente inspirador, proporcionando melhor desempenho em cada atividade.
+        Nosso objetivo, é realizar esta melhoria fornecendo produtos e serviços
+        adequados para cada projeto, de forma que a experiência durante todo
+        processo seja positiva e memorável.
+        <br /><br />
+        A qualidade do espaço de trabalho interfere diretamente na vida das
+        pessoas e nossa missão é garantir que, ao final de cada implantação,
+        todos os envolvidos neste processo sintam esta transformação.
       </p>
 
       <p class="sobre-description graphik18">
-      Nosso objetivo, é realizar esta melhoria para nossos clientes de forma que a experiência durante todo processo seja positiva e memorável. A qualidade do espaço de trabalho interfere diretamente na vida das pessoas e nossa missão é garantir, que ao final de cada implantação, a empresa e seus integrantes usufruam destas melhorias e tenham melhor qualidade de vida e desempenho.
+        Somos um time de pessoas que gostam do que fazem e de dedicação total a
+        satisfação de nossos clientes. É sempre um prazer participar destes
+        processos e ainda poder fazer amigos durante esta jornada.
       </p>
 
       <ul class="showroom-adress graphik16">
@@ -22,141 +37,140 @@
         <li class="gray">Vila Madalena</li>
       </ul>
 
-      <img class="img-showroom lazyload" :data-srcset="require('~/assets/images/escinter-showroom.png').srcSet" />
-
+      <img
+        class="img-showroom lazyload"
+        :data-srcset="require('~/assets/images/escinter-showroom.png').srcSet"
+      />
     </div>
   </div>
 </template>
 
 <style scoped>
 .carousel-cell {
-  width: calc( ( 100% - 0px ) / 3);
+  width: calc((100% - 0px) / 3);
 }
 
-  .content {
-    padding-top: 415px;
-    margin-bottom: 158px;
-  }
+.content {
+  padding-top: 415px;
+  margin-bottom: 158px;
+}
 
-  .servicos-title {
-    grid-column: 1/3;
-    margin-bottom: 32px;
+.servicos-title {
+  grid-column: 1/3;
+  margin-bottom: 32px;
+}
+
+.linha-div {
+  grid-column: 1/4;
+  height: 1px;
+  background-color: #dedcdb;
+}
+
+.sobre-description {
+  margin: 38px 0 34px 0;
+}
+
+.showroom-adress {
+  grid-column: 1/2;
+  align-self: end;
+  padding-bottom: 40px;
+}
+
+.img-showroom {
+  grid-column: 2/4;
+  margin-bottom: 40px;
+}
+
+.nossas-marcas-title {
+  grid-column: 1/3;
+  margin: 31px 0 27px 0;
+}
+
+.nossas-marcas-description {
+  grid-column: 1/3;
+  margin-bottom: 123px;
+}
+
+.marcas {
+  margin-bottom: 80px;
+}
+
+.last {
+  margin-bottom: 80px;
+}
+
+.marca-item-title {
+  margin-bottom: 20px;
+}
+
+.clientes-title {
+  grid-column: 1/3;
+  margin: 31px 0 81px 0;
+}
+
+.cliente-grid {
+  grid-column: 1/4;
+  margin-bottom: 80px;
+}
+
+.carousel-pag {
+  grid-column: 1/4;
+}
+
+.grid3-projetos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 28px;
+}
+
+@media screen and (max-width: 1020px) {
+  .carousel-cell {
+    width: calc(100vw - 40px);
+    margin-right: 20px;
   }
 
   .linha-div {
-    grid-column: 1/4;
-    height: 1px;
-    background-color: #DEDCDB;
+    grid-column: 1/3;
   }
 
   .sobre-description {
-    margin: 38px 0 34px 0;
+    grid-column: 1/3;
+    margin: 38px 0 0px 0;
   }
 
   .showroom-adress {
-    grid-column: 1/2;
-    align-self: end;
-    padding-bottom: 40px;
+    grid-column: 1/3;
+    grid-row: 6;
   }
 
   .img-showroom {
-    grid-column: 2/4;
-    margin-bottom: 40px;
-  }
-
-  .nossas-marcas-title {
-    grid-column: 1/3;
-    margin: 31px 0 27px 0;
-  }
-
-  .nossas-marcas-description {
-    grid-column: 1/3;
-    margin-bottom: 123px;
-  }
-
-  .marcas {
-    margin-bottom: 80px;
-  }
-
-  .last {
-    margin-bottom: 80px;
-  }
-
-  .marca-item-title {
-    margin-bottom: 20px;
-  }
-
-  .clientes-title {
-    grid-column: 1/3;
-    margin: 31px 0 81px 0;
-  }
-
-  .cliente-grid {
-    grid-column: 1/4;
-    margin-bottom: 80px;
-  }
-
-  .carousel-pag {
-    grid-column: 1/4;
-  }
-  
-  .grid3-projetos {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      grid-column-gap: 28px;
-  }
-
-  @media screen and (max-width: 1020px) {
-
-.carousel-cell {
-  width: calc(100vw - 40px);
-  margin-right: 20px;
-}
-
-
-    .linha-div {
-      grid-column: 1/3;
-    }
-
-    .sobre-description {
-      grid-column: 1/3;
-      margin: 38px 0 0px 0;
-    }
-
-    .showroom-adress {
-        grid-column: 1/3;
-        grid-row: 6;
-    }
-
-    .img-showroom {
     grid-column: 1/3;
     margin-top: 40px;
     margin-bottom: 16px;
-    }
+  }
 
-    .marcas {
-      grid-column: 1/3;
-      margin-bottom: 64px;
-    }
-
-    .cliente-grid {
-      grid-column: 1/3; 
-    }
-
-    .nossas-marcas-description {
+  .marcas {
+    grid-column: 1/3;
     margin-bottom: 64px;
   }
 
-    .content {
-      padding-top: 157px;
-    }
-
+  .cliente-grid {
+    grid-column: 1/3;
   }
+
+  .nossas-marcas-description {
+    margin-bottom: 64px;
+  }
+
+  .content {
+    padding-top: 157px;
+  }
+}
 </style>
 
 <script>
-import MenuMobile from '~/components/MenuMobile.vue'
-import ProjetoItem from '~/components/ProjetoItem.vue'
+import MenuMobile from "~/components/MenuMobile.vue";
+import ProjetoItem from "~/components/ProjetoItem.vue";
 
 export default {
   components: {
@@ -165,13 +179,13 @@ export default {
   },
   data() {
     return {
-      title: 'Sobre'
-    }
+      title: "Sobre"
+    };
   },
   head() {
     return {
       title: this.title
-    }
+    };
   }
-}
+};
 </script>
